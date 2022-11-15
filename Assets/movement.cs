83640ba8d908e83336a2eee7f925bea1ef83fd16
pichada00 <pichada.kon@gmail.com>
2022-11-15 21:00:11 +0700

    add TPS FPS 2.5D

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -12,11 +12,11 @@ public enum viewpoint
 public class movement : MonoBehaviour
 {
     private CharacterController controller;
-    [HideInInspector]public Animator animator;
+    [HideInInspector] public Animator animator;
     private Vector3 playerVelocity;
-    //[SerializeField] private bool groundedPlayer;
-    [SerializeField]private float playerSpeed = 3.5f;
-    [SerializeField]private float jumpHeight = 1.0f;
+    [SerializeField] private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 2.5f;
+    [SerializeField] private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
     //Run
@@ -38,69 +38,72 @@ public class movement : MonoBehaviour
     string state;
     public bool climbOn;
     float turnSmoothVelocity;
-    [SerializeField]private float rotationSpeed = 0.1f;
-    [SerializeField]private Transform cameraTransform;
+    [SerializeField] private float rotationSpeed = 0.1f;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Transform cameraFPS;
     private endbehav testbe;
     private climb Climb;
 
     public viewpoint Viewpoint;
-    private float horizontalMove;
-    public GameObject Camera;
-    public GameObject CameraSide;
+    private Quaternion lookLeft;
+    private Quaternion lookRight;
+
+    //
     private void Start()
     {
-        OnApplicationFocus(true);
         controller = gameObject.AddComponent<CharacterController>();
         rb = gameObject.AddComponent<Rigidbody>();
         animator = gameObject.GetComponent<Animator>();
         Climb = gameObject.GetComponent<climb>();
+        lookRight = transform.rotation;
+        lookLeft = lookRight * Quaternion.Euler(0, 180, 0);
     }
 
     void Update()
     {
-        if ( climbOn == true)
+        groundedPlayer = IsGround();
+        if (climbOn == true)
         {
-            if(Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 Debug.Log("cimbup");
                 animator.SetBool("Climb", false);
                 //testbe = animator.GetBehaviour<endbehav>();
                 //testbe.OnStateExit(animator, rb);
-            }            
-            
-        }else if(climbOn == false)
+            }
+
+        }
+        else if (climbOn == false)
         {
-            
+
             Climb.checkClimb();
             //Climb.enabled = true;
             //rb.isKinematic = false;
-           //rb.useGravity = true;
+            //rb.useGravity = true;
         }
 
         //groundedPlayer = IsGround();
         if (IsGround() && playerVelocity.y < 0)
         {
-            playerVelocity.y = 0f;
+            playerVelocity.y = -2f;
         }
-
         switch (Viewpoint)
         {
             case viewpoint.FPS:
                 FPSMove();
-                changeCameraFPS(1);
                 break;
             case viewpoint.TPS:
                 TPSMove();
-                changeCameraFPS(2);
                 break;
             case viewpoint.SC:
-                SideMove();
-                changeCameraFPS(3);
+                SCMove();
                 break;
-        }       
+        }
+        
+
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && IsGround() && climbOn==false)
+        if (Input.GetButtonDown("Jump") && IsGround() && climbOn == false)
         {
             Debug.Log("jump");
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
@@ -112,28 +115,42 @@ public class movement : MonoBehaviour
             playerVelocity.y += gravityValue * Time.deltaTime;
             controller.Move(playerVelocity * Time.deltaTime);
         }
-        
+
 
     }
 
-    public void FPSMove()
+    public void SCMove()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        move = transform.right * x + transform.forward * z;
-
-        if (move != Vector3.zero)
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (Input.GetAxis("Horizontal") > 0)
         {
-
-            state = IsRun() ? "Run" : "Walk";
-            stateCharacter(state);
-
-
+            transform.rotation = lookRight;
         }
-        else if (move == Vector3.zero)
+        if (Input.GetAxis("Horizontal") < 0)
         {
-            stateCharacter("Idle");
+            transform.rotation = lookLeft;
+        }
+        //direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+        //direction = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * direction;
+        //direction.Normalize();
+        if (climbOn == false)
+        {
+            if (move != Vector3.zero)
+            {
+                //Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+
+                //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 360 * Time.deltaTime);
+
+                state = IsRun() ? "Run" : "Walk";
+                stateCharacter(state);
+
+
+            }
+            else if (move == Vector3.zero)
+            {
+                stateCharacter("Idle");
+            }
         }
     }
 
@@ -164,54 +181,35 @@ public class movement : MonoBehaviour
         }
     }
 
-    public void SideMove()
+    public void FPSMove()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * playerSpeed;
-    }
-
-    public void changeCameraFPS(int i)
-    {
-        switch (i)
+        move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        transform.rotation = Quaternion.Euler(0, cameraFPS.rotation.eulerAngles.y, 0);
+        transform.Translate(move * Time.deltaTime * playerSpeed);
+        if (climbOn == false)
         {
-            case 1:
-                cameraTransform.gameObject.SetActive(false);
-                CameraSide.SetActive(false);
-                Camera.SetActive(true);
-                break;
-            case 2:
-                cameraTransform.gameObject.SetActive(true);
-                CameraSide.SetActive(false);
-                Camera.SetActive(false);
-                break;
-            case 3:
-                cameraTransform.gameObject.SetActive(false);
-                CameraSide.SetActive(true);
-                Camera.SetActive(false);
-                break;
+            if (move != Vector3.zero)
+            {
+                //Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+
+                //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 360 * Time.deltaTime);
+
+                state = IsRun() ? "Run" : "Walk";
+                stateCharacter(state);
+
+
+            }
+            else if (move == Vector3.zero)
+            {
+                stateCharacter("Idle");
+            }
         }
-        
     }
     public void stateCharacter(string state)
     {
         switch (Viewpoint)
         {
             case viewpoint.FPS:
-                switch (state)
-                {
-                    case "Idle":
-                        animator.SetFloat("speed", 0f);
-                        break;
-                    case "Walk":
-                        animator.SetFloat("speed", 2.0f);
-                        controller.Move(direction * Time.deltaTime * playerSpeed);
-                        break;
-                    case "Run":
-                        controller.Move(direction * Time.deltaTime * runSpeed);
-                        animator.SetFloat("speed", 4.3f);
-                        break;
-                }
-                break;
-            case viewpoint.TPS:
                 switch (state)
                 {
                     case "Idle":
@@ -227,7 +225,37 @@ public class movement : MonoBehaviour
                         break;
                 }
                 break;
+            case viewpoint.TPS:
+                switch (state)
+                {
+                    case "Idle":
+                        animator.SetFloat("speed", 0f);
+                        break;
+                    case "Walk":
+                        animator.SetFloat("speed", 2.0f);
+                        controller.Move(direction * Time.deltaTime * playerSpeed);
+                        break;
+                    case "Run":
+                        controller.Move(direction * Time.deltaTime * runSpeed);
+                        animator.SetFloat("speed", 4.3f);
+                        break;
+                }
+                break;
             case viewpoint.SC:
+                switch (state)
+                {
+                    case "Idle":
+                        animator.SetFloat("speed", 0f);
+                        break;
+                    case "Walk":
+                        animator.SetFloat("speed", 2.0f);
+                        controller.Move(move * Time.deltaTime * playerSpeed);
+                        break;
+                    case "Run":
+                        controller.Move(move * Time.deltaTime * runSpeed);
+                        animator.SetFloat("speed", 4.3f);
+                        break;
+                }
                 break;
         }
         
@@ -245,11 +273,11 @@ public class movement : MonoBehaviour
         }
     }
 
-    
+
 
     bool IsGround()
     {
-        return Physics.CheckSphere(checkground.position, .1f, ground);
+        return Physics.CheckSphere(checkground.position, .4f, ground);
     }
 
     bool IsRun()
@@ -262,5 +290,6 @@ public class movement : MonoBehaviour
         Debug.Log("isclimb");
         climbOn = false;
     }
-    
+
+
 }
